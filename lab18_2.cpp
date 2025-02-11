b389d01a8a33d2caefaf382cd229443f0b7cab2b
notpeddy <panisa.krt@gmail.com>
2025-02-11 21:33:20 +0700

    Update lab18_2.cpp

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,17 @@
-#include<iostream>
-using namespace std;
+	#include<iostream>
+	using namespace std;
 
-struct Rect{
-	double x,y,w,h;
-};
+	struct Rect{
+		double x,y,w,h;
+	};
 
-double overlap(){
+	double overlap(Rect *R1, Rect *R2){
+		double right = min(R1->x+R1->w , R2->x+R2->w) ;
+		double left = max(R1->x,R2->x);
+		double top = min(R1->y,R2->y) ;
+		double bottom =  max(R1->y-R1->h , R2->y-R2->h);
 
-
-
-}
+		if(left<right && top>bottom) return (right - left)* (top - bottom);
+		else return 0;
+		
+	}
